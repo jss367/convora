d6dc2cd300f82c7587078551d629d88430496768
Julius Simonelli <julius.simonelli@gmail.com>
2024-06-27 00:22:17 -0700

    wip

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,17 +3,25 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-  root: path.resolve(__dirname, ''),
-  build: {
-    outDir: path.resolve(__dirname, '../dist'),
-  },
-  server: {
-    port: 5173,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [react()],
+    root: path.resolve(__dirname, ''),
+    build: {
+        outDir: path.resolve(__dirname, '../dist'),
     },
-  },
+    server: {
+        port: 5173,
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    css: {
+        postcss: {
+            plugins: [
+                require('tailwindcss'),
+                require('autoprefixer'),
+            ],
+        },
+    },
 })
